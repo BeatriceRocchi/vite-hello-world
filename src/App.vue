ecb950062429557971c797064172968d9238b1c9
BeatriceRocchi <bea.rocchi93@gmail.com>
2024-03-20 14:46:07 +0100

    [add] logo

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ export default {
     return {
       title: "Vite Hello World",
       text: 'Vite (French word for "quick", pronounced /vit/, like "veet") is a build tool that aims to provide a faster and leaner development experience for modern web projects.',
+      logoUrl: "https://vitejs.dev/logo-with-shadow.png",
     };
   },
 };
@@ -11,6 +12,7 @@ export default {
 
 <template>
   <div class="box-container">
+    <img :src="logoUrl" alt="Logo Vite" />
     <h1>{{ title }}</h1>
     <p>{{ text }}</p>
   </div>
@@ -22,10 +24,18 @@ export default {
   text-align: center;
 }
 
+img {
+  height: 350px;
+}
+
 h1 {
   font-size: 4rem;
   margin-bottom: 20px;
   text-shadow: 0 0 40px rgb(189, 52, 254);
+}
+
+h1:hover {
+  color: rgb(255, 220, 52);
 }
 
 p {
