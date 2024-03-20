798e04f973bb6f3a2be9c88fcd74a1fa1ca7cab7
BeatriceRocchi <bea.rocchi93@gmail.com>
2024-03-20 14:34:25 +0100

    [add] global and scoped styles

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,34 @@
 <script>
 export default {
   data() {
-    return {};
+    return {
+      title: "Vite Hello World",
+      text: 'Vite (French word for "quick", pronounced /vit/, like "veet") is a build tool that aims to provide a faster and leaner development experience for modern web projects.',
+    };
   },
 };
 </script>
 
 <template>
-  <div></div>
+  <div class="box-container">
+    <h1>{{ title }}</h1>
+    <p>{{ text }}</p>
+  </div>
 </template>
 
-<style></style>
+<style scoped>
+.box-container {
+  padding: 0 300px;
+  text-align: center;
+}
+
+h1 {
+  font-size: 4rem;
+  margin-bottom: 20px;
+  text-shadow: 0 0 40px rgb(189, 52, 254);
+}
+
+p {
+  font-size: 2rem;
+}
+</style>
